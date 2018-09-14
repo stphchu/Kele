0d98fe7cbe656692fc0a3e71d6c387ff34edcd81
Stephanie <esceeaitchyu@gmail.com>
2018-09-14 09:40:33 -0700

    Completion of User Story 6: Remaining Checkpoints

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -17,4 +17,10 @@ module Roadmap
      JSON.parse(response.body)
   end
 
+#GET_REMAINING_CHECKPOINTS
+  def get_remaining_checkpoints(chain_id)
+     response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token })
+     JSON.parse(response.body)
+  end
+
 end
